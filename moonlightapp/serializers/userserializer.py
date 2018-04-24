4302958f501d7718983d14d74f4ff03506366320
KlwntSingh <kulwantbughipura@gmail.com>
2018-04-24 11:11:54 -0300

    temporary files deleted

--- moonlightapp/serializers/userserializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from moonlightapp.models.usermodel import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ("name",)
