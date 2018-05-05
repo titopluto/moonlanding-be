2b6de5ce7f3a8b05844d1ea9fb246ad5dcc99128
KlwntSingh <kulwantbughipura@gmail.com>
2018-05-05 15:58:56 -0300

    guardian backends reverted

--- backend/settings.py
+++ backend/settings.py
@@ -37,15 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'guardian',
     'corsheaders',
     'moonlightapp',
 ]
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend', # this is default
-    'guardian.backends.ObjectPermissionBackend',
-)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
